deec1a1300cd6f9fde9576559ce4a5f3aad1f9ed
Chester Eloise Ragas <ce.ragas@seiko-it.com.ph.local>
2018-07-05 15:37:29 +0800

    commit to update

--- app/Http/Controllers/QCDB/OQCGroupByController.php
+++ app/Http/Controllers/QCDB/OQCGroupByController.php
@@ -563,8 +563,7 @@ class OQCGroupByController extends Controller
         return $ins;
     }
 
-    private function DPPMTables($req,$join)
-    {
+    private function DPPMTables($req,$join){
         $g1 = ''; $g2 = ''; $g3 = '';
         $g1c = ''; $g2c = ''; $g3c = '';
         $date_inspected = ''; $sub_date_inspected = '';
@@ -1508,7 +1507,6 @@ class OQCGroupByController extends Controller
 
         })->download('xls');
     }
-
 
     private function insertToReportsv2($details,$type)
     {
