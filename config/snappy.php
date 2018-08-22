17b96517f71dcab66e273c0a1edf7b14481272fc
Kurt Dela Rosa <kurtcreampie@gmail.com>
2018-08-22 14:02:53 +0800

    Fix iqc pdf error

--- config/snappy.php
+++ config/snappy.php
@@ -6,14 +6,14 @@ return array(
     'pdf' => array(
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltopdf',
-        'timeout' => false,
+        'timeout' => 3600,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltoimage',
-        'timeout' => false,
+        'timeout' => 3600,
         'options' => array(),
         'env'     => array(),
     ),
